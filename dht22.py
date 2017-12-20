ea3327a6cb4723866eaf2c2f8ae8e79a77b185d6
Jim Englert <englert.james@gmail.com>
2017-12-20 16:23:31 +0000

    A script that uploads some stuff to S3.

--- dht22.py
+++ dht22.py
@@ -1,29 +1,16 @@
 
-"""  
-Temperature/Humidity monitor using Raspberry Pi and DHT22. 
-Data is displayed at thingspeak.com
-Original author: Mahesh Venkitachalam at electronut.in 
-Modified by Adam Garbo on December 1, 2016 
-""" 
 import sys 
 import RPi.GPIO as GPIO 
 from time import sleep 
 import Adafruit_DHT 
-import urllib2 
+
 def getSensorData(): 
 	RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 23) 
 	temp = '{0:0.1f}'.format((T * 1.8 + 32))
 	return ('{0:0.1f}'.format(RH), temp) 
 def main(): 
-	print 'starting...' 
- 	while True: 
-		try: 
-			RH, T = getSensorData() 
-			print "Humidity:" + RH + " Temperature:" + T
-			sleep(3)  
-	    	except: 
-			print 'exiting.' 
-			break 
+	RH, T = getSensorData() 
+        print RH + "-" + T
 
 # call main 
 if __name__ == '__main__':
