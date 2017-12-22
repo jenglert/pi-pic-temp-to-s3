7a962b8062cf547530763f48c83c141e351dcb4e
Jim Englert <englert.james@gmail.com>
2017-12-22 15:38:52 +0000

    Explicit paths

--- capture.sh
+++ capture.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-temp_and_humidity=$(python dht22.py)
+temp_and_humidity=$(/usr/bin/python /home/pi/pi-pic-temp-to-s3/dht22.py)
 
 file_path=$(raspistill -o "/tmp/pic-%d-$temp_and_humidity.jpg" -ts -q 8 -h 1080 -w 1280 -v 2>&1 | grep "Opening output file " | awk '{ gsub(/Opening output file /, ""); print }')
 
@@ -10,7 +10,7 @@ file=$(echo $file_path | awk '{gsub("/tmp/", ""); print}')
 
 file_size=$(stat --printf="%s" $file_path)
 
-aws s3api put-object --bucket pi-pics --body "$file_path" --key "$file"
+/home/pi/.local/bin/aws s3api put-object --bucket pi-pics --body "$file_path" --key "$file"
 
 echo "Wrote $file_size bytes to S3 key $file"
 
